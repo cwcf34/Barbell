69224eeb9ad62bd007f109bf1646eee41e54bac0
cjafk2 <calebalbertson@calebs-mbp.centurylink.net>
2017-03-07 17:41:50 -0600

    Committing back-end work from Friday -serialization -coredata schema

---
+++ Barbell/Barbell/Models/Routine+CoreDataProperties.swift
@@ -0,0 +1,59 @@
+//
+//  Routine+CoreDataProperties.swift
+//  Barbell
+//
+//  Created by Caleb Albertson on 2/24/17.
+//  Copyright © 2017 Team Barbell. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Routine {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Routine> {
+        return NSFetchRequest<Routine>(entityName: "Routine")
+    }
+
+    @NSManaged public var id: Int16
+    @NSManaged public var name: String?
+    @NSManaged public var creator: User?
+    @NSManaged public var users: NSSet?
+    @NSManaged public var workouts: NSSet?
+
+}
+
+// MARK: Generated accessors for users
+extension Routine {
+
+    @objc(addUsersObject:)
+    @NSManaged public func addToUsers(_ value: User)
+
+    @objc(removeUsersObject:)
+    @NSManaged public func removeFromUsers(_ value: User)
+
+    @objc(addUsers:)
+    @NSManaged public func addToUsers(_ values: NSSet)
+
+    @objc(removeUsers:)
+    @NSManaged public func removeFromUsers(_ values: NSSet)
+
+}
+
+// MARK: Generated accessors for workouts
+extension Routine {
+
+    @objc(addWorkoutsObject:)
+    @NSManaged public func addToWorkouts(_ value: Workout)
+
+    @objc(removeWorkoutsObject:)
+    @NSManaged public func removeFromWorkouts(_ value: Workout)
+
+    @objc(addWorkouts:)
+    @NSManaged public func addToWorkouts(_ values: NSSet)
+
+    @objc(removeWorkouts:)
+    @NSManaged public func removeFromWorkouts(_ values: NSSet)
+
+}
---
+++ Barbell/Barbell/Models/Workout+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Workout+CoreDataClass.swift
+//  Barbell
+//
+//  Created by Caleb Albertson on 2/24/17.
+//  Copyright © 2017 Team Barbell. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Workout: NSManagedObject {
+
+}
