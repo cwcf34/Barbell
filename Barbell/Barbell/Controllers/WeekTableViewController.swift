1145abd2f14ac4534abbb78c878c4e074087fee8
cwcf34 <cwcf34@mail.missouri.edu>
2017-03-21 19:36:09 -0500

    Added UI for Exercises

--- Barbell/Barbell/Controllers/WeekTableViewController.swift
+++ Barbell/Barbell/Controllers/WeekTableViewController.swift
@@ -1,17 +1,14 @@
 //
-//  CreateWorkoutTableViewController.swift
+//  WeekTableViewController.swift
 //  Barbell
 //
-//  Created by Cody Cameron on 3/7/17.
+//  Created by Cody Cameron on 3/21/17.
 //  Copyright © 2017 Team Barbell. All rights reserved.
 //
 
 import UIKit
-import CoreData
 
-class CreateWorkoutTableViewController: UITableViewController {
-    
-    //var routines = [Routine]()
+class WeekTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,32 +34,23 @@ class CreateWorkoutTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 7
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Test")
+        self.performSegue(withIdentifier: "showExercisesSegue", sender: self)
     }
 
-    
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "addWorkout", for: indexPath) 
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
         // Configure the cell...
-        
+
         return cell
     }
- 
-    /*func loadRoutines() -> [Routine]{
-        
-        
-        //CoreData
-        let fetchRequest = NSFetchRequest<Routine>(entityName: "Routine")
-        do{
-            let foundRoutine = try CoreDataController.getContext().fetch(fetchRequest)
-            return foundRoutine
-        }catch{
-            print("we messed this up")
-        }
-        return [Routine]()
-        
-    }*/
+    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -108,41 +96,5 @@ class CreateWorkoutTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    
-    
-    /*Logic for searching through workouts */
-    
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        filterPlayers = allPlayers?.filter({ (players: Player) -> Bool in
-//            return players.firstName?.lowercased().range(of: searchText.lowercased()) != nil
-//        })
-//        
-//        if searchText != ""{
-//            shouldShowSearch = true
-//            self.tableView.reloadData()
-//        }
-//        else{
-//            shouldShowSearch = false
-//            self.tableView.reloadData()
-//        }
-//    }
-//    
-//    
-//    func createSearchbar() {
-//        searchBar.showsCancelButton = false
-//        searchBar.placeholder = "Enter search"
-//        searchBar.delegate = self
-//        
-//        self.navigationItem.titleView = searchBar
-//    }
-//    
-//    func searchBarSearchButtonClicked(searchBar: UISearchBar){
-//        shouldShowSearch = true
-//        searchBar.endEditing(true)
-//        self.tableView.reloadData()
-//    }
-    
-    /*End Search Logic*/
 
 }
