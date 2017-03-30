21ee0a7cf7514088005b67fb4cee14011bc8ed17
cwcf34 <cwcf34@mail.missouri.edu>
2017-03-30 10:18:11 -0500

    Finished UI for adding routine
    Finished UI, still need some logic for fetching from DB and passing
    between viewControllers

--- Barbell/Barbell/Controllers/AllExercisesTableViewController.swift
+++ Barbell/Barbell/Controllers/AllExercisesTableViewController.swift
@@ -85,7 +85,11 @@ class AllExercisesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return allExercises[section].muscleGroup
-        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "addStatsSegue", sender: self)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- Barbell/Barbell/Controllers/WeekTableViewController.swift
+++ Barbell/Barbell/Controllers/WeekTableViewController.swift
@@ -38,7 +38,6 @@ class WeekTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Test")
         self.performSegue(withIdentifier: "addExercisesSegue", sender: self)
     }
 
