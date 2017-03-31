3ad2b305401b8a12b7d52bc7e6ffe03f3a4dff54
cjafk2 <calebalbertson@calebs-mbp.centurylink.net>
2017-03-31 13:48:04 -0500

    moving stuff from my old branch over

--- Barbell/Barbell/Controllers/DataAccess.swift
+++ Barbell/Barbell/Controllers/DataAccess.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import CoreData
 
 
 public class DataAccess {
@@ -152,4 +153,83 @@ public class DataAccess {
             return false
         }
     }
+    
+    class func getUserfromRedis(email : String){
+        var request = URLRequest(url: URL(string: apiURL + "user/\(email)/")!)
+        let user : User = NSEntityDescription.insertNewObject(forEntityName: "User", into: CoreDataController.getContext()) as! User
+        request.httpMethod = "GET"
+        var responseString = ""
+        
+        let headers = [
+            "content-type": "application/json"
+        ]
+        
+        request.allHTTPHeaderFields = headers
+        
+        let sem = DispatchSemaphore(value: 0)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            responseString = String(data: data, encoding: .utf8)!
+            
+            print("Get user response " + responseString)
+            
+            
+            sem.signal()
+        }
+        
+        task.resume()
+        sem.wait()
+        
+        let tokens = responseString.components(separatedBy: ",")
+        //var token = [String]()
+        for i in tokens{
+            var token = i.components(separatedBy: ":")
+            if token[0] == "name"{
+                let nameTokens = token[1].components(separatedBy: " ")
+                user.fname = nameTokens[0]
+                user.lname = nameTokens[1]
+                if token[0] == "email"{
+                    user.email = token[1]
+                }
+                if token[0] == "age"{
+                    user.age = Int16(token[1])!
+                }
+                if token[0] == "weight"{
+                    user.weight = Int16(token[1])!
+                }
+                if token[0] == "squat"{
+                    user.squat = Int16(token[1])!
+                }
+                if token[0] == "bench"{
+                    user.bench = Int16(token[1])!
+                }
+                if token[0] == "deadlift"{
+                    user.deadlift = Int16(token[1])!
+                }
+                if token[0] == "cleanjerk"{
+                    user.cleanAndJerk = Int16(token[1])!
+                }
+                if token[0] == "snatch"{
+                    user.snatch = Int16(token[1])!
+                }
+                if token[0] == "workoutsCompleted"{
+                    user.workoutsCompleted = Int16(token[1])!
+                }
+            }
+            
+        }
+        CoreDataController.saveContext()
+        return
+    }
+
 }
