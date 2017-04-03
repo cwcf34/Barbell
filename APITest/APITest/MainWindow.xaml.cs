90a19e068198f887c9abd230cb62cc8c666a2bde
dbmfzf <dbmfzf@mail.missouri.edu>
2017-04-02 20:00:55 -0500

    Create APITest & add controller to test api's token auth

---
+++ APITest/APITest/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APITest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static string[] testArray = { "Security", "All" };
+        private List<string> testList = new List<string>(testArray);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Populate the combobox
+            foreach(string item in testList)
+            {
+                testList_comboBox.Items.Add(item);
+            }
+        }
+
+        private async void runTest_button_Click(object sender, RoutedEventArgs e)
+        {
+            //Clear the output textblock
+            output_textBlock.Text = "Running...\n";
+
+            string testOutput = "";
+            switch (testList_comboBox.Text)
+            {
+                case "Security":
+                    testOutput = await Tests.RunSecurityTest();
+                    break;
+                case "All":
+                    testOutput = Tests.RunAllTests();
+                    break;
+                default:
+                    testOutput = "Please make a selection from the combobox.";
+                    break;
+            }
+
+            output_textBlock.Text += testOutput;
+        }
+
+
+    }
+}
---
+++ BBAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using BBAPI.Helpers;
+using System.Security.Claims;
+
+namespace BBAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        /// <summary>
+        /// This function is just to test the API's ability to validate a token
+        /// </summary>
+        /// <returns>S</returns>
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult GetTest()
+        {
+            return Ok("Success");
+        }
+    }
+}
