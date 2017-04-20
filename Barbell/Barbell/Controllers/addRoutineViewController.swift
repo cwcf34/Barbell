e9a0c1c927359b5e93bd7801d51f01555eb8df35
cwcf34 <cwcf34@mail.missouri.edu>
2017-04-20 18:39:56 -0500

    Test Commit for UI updates

--- Barbell/Barbell/Controllers/addRoutineViewController.swift
+++ Barbell/Barbell/Controllers/addRoutineViewController.swift
@@ -27,6 +27,8 @@ class addRoutineViewController: UIViewController, UITableViewDataSource, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
         goingForwards = false
         var count : Int = Int(routinePassed.numberOfWeeks)
 
