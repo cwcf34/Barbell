0c21347f9d2f9d0e8242535b45588e77f278b60d
Curtis Markway <cmarkway14@gmail.com>
2017-04-22 17:18:03 -0500

    Added history

---
+++ Barbell/Barbell/Controllers/HistoryTableViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CreateWorkoutTableViewController.swift
+//  Barbell
+//
+//  Created by Cody Cameron on 3/7/17.
+//  Copyright © 2017 Team Barbell. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class HistoryTableViewController: UITableViewController {
+    
+    var foundRoutines = [Routine]()
+    var routine : Routine!
+    var routineSearchResults = [Routine]()
+    var didDelete: Bool!
+    var shouldBeginEditing = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //might see if we can get to the hex color
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = CoreDataController.getContext()
+        didDelete = false
+        
+        let fetchRequest = NSFetchRequest<Routine>(entityName: "Routine")
+        do{
+            foundRoutines = try context.fetch(fetchRequest)
+            
+        }catch{
+            print("Bad getExercise query")
+        }
+        for routine in foundRoutines{
+            if(routine.name == nil || routine.name == "") {
+                CoreDataController.getContext().delete(routine)
+                didDelete = true;
+                break;
+            }
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+            return 1 + foundRoutines.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell?
+        
+            cell = tableView.dequeueReusableCell(withIdentifier: "workoutCell", for: indexPath)
+        
+                cell?.textLabel?.text = foundRoutines[indexPath.row-1].name
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+                routine = foundRoutines[indexPath.row-1]
+                self.performSegue(withIdentifier: "finishedWorkouts", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "finishedWorkouts"){
+            let viewController = segue.destination as! StartRoutineTableViewController
+            viewController.routinePassed = routine
+        }
+    }
+}
---
+++ Barbell/Barbell/Controllers/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  Barbell
+//
+//  Created by Curtis Markway on 4/22/17.
+//  Copyright © 2017 Team Barbell. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+}
