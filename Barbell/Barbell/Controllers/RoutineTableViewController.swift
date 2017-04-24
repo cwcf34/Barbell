5c26cf2756069321ee10a81c0bd63fb745992901
Curtis Markway <cmarkway14@gmail.com>
2017-04-23 21:21:13 -0500

    Fixed achievements

--- Barbell/Barbell/Controllers/RoutineTableViewController.swift
+++ Barbell/Barbell/Controllers/RoutineTableViewController.swift
@@ -38,8 +38,10 @@ class RoutineTableViewController: UITableViewController, UISearchBarDelegate {
             for foundRoutine in foundRoutines{
                 if foundRoutine.isFinished == true{
                     foundRoutines.remove(at: counter)
+                }else{
+                    counter += 1    
                 }
-                counter += 1
+                
             }
             
         }catch{
